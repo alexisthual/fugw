5b629440bf4b14582ea2120c9002635126433889
alexis <alexisthual@gmail.com>
2022-02-17 23:04:56 +0100

    Fix method name in base model

--- src/fugw/utils.py
+++ src/fugw/utils.py
@@ -5,5 +5,5 @@ class BaseModel(BaseEstimator, TransformerMixin):
     def fit(self, source_data, target_data):
         return None
 
-    def predict(self, source_map):
+    def transform(self, source_map):
         return None
