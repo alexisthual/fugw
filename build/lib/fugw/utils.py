dc38e9743b1fef4ca39c9113f2413c9979f02099
Alexis Thual <alexisthual@gmail.com>
2022-10-05 11:05:04 +0200

    Remove build folder

--- build/lib/fugw/utils.py
+++
@@ -1,9 +0,0 @@
-from sklearn.base import BaseEstimator, TransformerMixin
-
-
-class BaseModel(BaseEstimator, TransformerMixin):
-    def fit(self, source_data, target_data):
-        return None
-
-    def transform(self, source_map):
-        return None
