9218b20bb4787603e3df956973fdfc5d2687144f
Alexis Thual <alexisthual@gmail.com>
2022-11-01 15:32:16 +0100

    Rework main class arguments

--- src/fugw/fugw.py
+++ src/fugw/fugw.py
@@ -10,61 +10,68 @@ from fugw.utils import BaseModel
 class FUGW(BaseModel):
     def __init__(
         self,
-        px=None,
-        py=None,
         alpha=1,
         rho=20,
         eps=1e-2,
         uot_solver="sinkhorn",
         reg_mode="joint",
-        init_plan=None,
-        init_duals=None,
         verbose=False,
         early_stopping_threshold=1e-6,
         **kwargs,
     ):
         # Save model arguments
-        self.px = px
-        self.py = py
         self.alpha = alpha
         self.rho = rho
         self.eps = eps
         self.uot_solver = uot_solver
         self.reg_mode = reg_mode
-        self.init_plan = init_plan
-        self.init_duals = init_duals
         self.verbose = verbose
         self.early_stopping_threshold = early_stopping_threshold
 
     def fit(
         self,
-        source_data,
-        target_data,
-        source_kernel=None,
-        target_kernel=None,
+        source_features,
+        target_features,
+        source_geometry=None,
+        target_geometry=None,
+        source_weights=None,
+        target_weights=None,
+        init_plan=None,
+        init_duals=None,
         **kwargs,
     ):
         """
-        Compute transport plan between source and target individuals
-        using functional contrast maps and anatomies.
+        Compute transport plan between source and target distributions
+        using feature maps and geometries.
+        In our case, feature maps are fMRI contrast maps,
+        and geometries are that of the cortical meshes of individuals
+        under study.
 
         Parameters
         ----------
-        source_data: ndarray(n_samples, n1)
-            Contrast maps for source subject.
-            n_samples is the number of contrast maps, it should
+        source_features: ndarray(n_features, n1)
+            Feature maps for source subject.
+            n_features is the number of contrast maps, it should
             be the same for source and target data.
             n1 is the number of nodes on the source graph, it
             can be different from n2, the number of nodes on the
             target graph.
-        target_data: ndarray(n_samples, n2)
-            Contrast maps for target subject
-        source_kernel: ndarray(n1, n1)
-            Kernel matrix representing anatomical similarities
-            between nodes of source subject mesh
-        target_kernel: ndarray(n2, n2)
-            Kernel matrix representing anatomical similarities
-            between nodes of target subject mesh
+        target_features: ndarray(n_features, n2)
+            Feature maps for target subject.
+        source_geometry: ndarray(n1, n1)
+            Kernel matrix of anatomical distances
+            between nodes of source mesh
+        target_geometry: ndarray(n2, n2)
+            Kernel matrix of anatomical distances
+            between nodes of target mesh
+        source_weights: ndarray(n1) or None
+            Distribution weights of source nodes.
+            Should sum to 1. If None, eahc node's weight
+            will be set to 1 / n1.
+        target_weights: ndarray(n1) or None
+            Distribution weights of target nodes.
+            Should sum to 1. If None, eahc node's weight
+            will be set to 1 / n2.
 
         Returns
         -------
@@ -81,18 +88,33 @@ class FUGW(BaseModel):
             rho_x, rho_y = self.rho
         else:
             raise ValueError(
-                "Invalid value of rho. Must be either a scalar or a tuple of two scalars."
+                "Invalid value of rho. Must be either a scalar or a tuple of"
+                " two scalars."
             )
 
-        Fs = torch.from_numpy(source_data.T).type(dtype)
-        Ft = torch.from_numpy(target_data.T).type(dtype)
+        # Set weights if they were not set by user
+        if source_weights is None:
+            source_weights = (
+                torch.ones(source_features.shape[1]).type(dtype)
+                / source_features.shape[1]
+            )
+
+        if target_weights is None:
+            target_weights = (
+                torch.ones(target_features.shape[1]).type(dtype)
+                / target_features.shape[1]
+            )
+
+        # Compute distance matrix between features
+        Fs = torch.from_numpy(source_features.T).type(dtype)
+        Ft = torch.from_numpy(target_features.T).type(dtype)
         K = torch.cdist(Fs, Ft, p=2)
 
         # Load anatomical kernels to GPU
         # and normalize them
-        Gs = torch.from_numpy(source_kernel).type(dtype)
+        Gs = torch.from_numpy(source_geometry).type(dtype)
         Gs = Gs / Gs.max()
-        Gt = torch.from_numpy(target_kernel).type(dtype)
+        Gt = torch.from_numpy(target_geometry).type(dtype)
         Gt = Gt / Gt.max()
 
         # Create model
@@ -100,22 +122,22 @@ class FUGW(BaseModel):
 
         # Compute transport plan
         res = model.solver(
+            px=source_weights,
+            py=target_weights,
+            D=K,
             X=Gs,
             Y=Gt,
-            px=self.px,
-            py=self.py,
-            D=K,
             alpha=self.alpha,
             rho_x=rho_x,
             rho_y=rho_y,
             eps=self.eps,
             uot_solver=self.uot_solver,
             reg_mode=self.reg_mode,
-            init_plan=self.init_plan,
-            init_duals=self.init_duals,
+            early_stopping_threshold=self.early_stopping_threshold,
+            init_plan=init_plan,
+            init_duals=init_duals,
             log=False,
             verbose=self.verbose,
-            early_stopping_threshold=self.early_stopping_threshold,
         )
 
         self.pi = res[0].detach().cpu().numpy()
@@ -125,16 +147,16 @@ class FUGW(BaseModel):
         if use_cuda:
             torch.cuda.empty_cache()
 
-        return self
+        return res
 
-    def transform(self, source_data):
+    def transform(self, source_features):
         """
         Transport source contrast map using fitted OT plan.
         Use GPUs if available.
 
         Parameters
         ----------
-        source_data: ndarray(n_samples, n1) or ndarray(n1)
+        source_features: ndarray(n_samples, n1) or ndarray(n1)
             Contrast map for source subject
 
         Returns
@@ -150,28 +172,31 @@ class FUGW(BaseModel):
             raise ("Model should be fitted before calling transform")
 
         is_one_dimensional = False
-        if source_data.ndim == 1:
+        if source_features.ndim == 1:
             is_one_dimensional = True
-            source_data = source_data.reshape(1, -1)
-        if source_data.ndim > 2:
+            source_features = source_features.reshape(1, -1)
+        if source_features.ndim > 2:
             raise ValueError(
-                "source_data has too many dimensions: " f"{source_data.ndim}"
+                "source_features has too many dimensions: "
+                f"{source_features.ndim}"
             )
 
         # Move data to GPU
         pi_torch = torch.from_numpy(self.pi).type(dtype)
-        source_data_torch = torch.from_numpy(source_data).type(dtype)
+        source_features_torch = torch.from_numpy(source_features).type(dtype)
 
         # Transform data
         transformed_data_torch = (
-            pi_torch.T @ source_data_torch.T / pi_torch.sum(dim=0).reshape(-1, 1)
+            pi_torch.T
+            @ source_features_torch.T
+            / pi_torch.sum(dim=0).reshape(-1, 1)
         ).T
 
         # Move transformed data back to CPU
         transformed_data = transformed_data_torch.detach().cpu().numpy()
 
         # Free allocated GPU memory
-        del pi_torch, source_data_torch
+        del pi_torch, source_features_torch
         if use_cuda:
             torch.cuda.empty_cache()
 
@@ -180,30 +205,30 @@ class FUGW(BaseModel):
 
         return transformed_data
 
-    def score(self, source_data, target_data):
+    def score(self, source_features, target_features):
         """
         Transport source contrast maps using fitted OT plan
         and compute correlation with actual target contrast maps.
 
         Parameters
         ----------
-        source_data: ndarray(n_samples, n1)
+        source_features: ndarray(n_samples, n1)
             Contrast maps for source subject
-        target_data: ndarray(n_samples, n2)
+        target_features: ndarray(n_samples, n2)
             Contrast maps for target subject
 
         Returns
         -------
         score: float
-            Mean correlation across features of self.transform(source_data)
-            and target_data
+            Mean correlation across features of self.transform(source_features)
+            and target_features
         """
 
-        transported_data = self.transform(source_data)
+        transported_data = self.transform(source_features)
 
         score = np.mean(
             [
-                pearsonr(transported_data[i, :], target_data[i, :])[0]
+                pearsonr(transported_data[i, :], target_features[i, :])[0]
                 for i in range(transported_data.shape[0])
             ]
         )
--- tests/test_fugw.py
+++ tests/test_fugw.py
@@ -1,46 +1,40 @@
-from fugw.fugw import FUGW
 import numpy as np
+from fugw import FUGW
 from sklearn.metrics import pairwise_distances
 
 
 def test_fugw_alignment():
-    # Generate random data for source and target for training
+    # Generate random training data for source and target
     np.random.seed(100)
-    n_voxels = 1000
+    n_voxels = 100
     n_train = 10
     n_test = 50
 
-    source_data_train = np.random.rand(n_train, n_voxels)
-    target_data_train = np.random.rand(n_train, n_voxels)
+    source_features_train = np.random.rand(n_train, n_voxels)
+    target_features_train = np.random.rand(n_train, n_voxels)
+
     source_embedding = np.random.rand(n_voxels, 3)
     target_embedding = np.random.rand(n_voxels, 3)
-    source_kernel = pairwise_distances(source_embedding)
-    target_kernel = pairwise_distances(target_embedding)
-
-    print(
-        f"""Source and target data shapes:
-{source_data_train.shape}
-{target_data_train.shape}"""
-    )
-
-    print(
-        f"""Source and target kernel shapes:
-{source_kernel.shape}
-{target_kernel.shape}"""
-    )
+    source_geometry = pairwise_distances(source_embedding)
+    target_geometry = pairwise_distances(target_embedding)
 
     fugw = FUGW()
-    fugw.fit(source_data_train, target_data_train, source_kernel, target_kernel)
+    fugw.fit(
+        source_features_train,
+        target_features_train,
+        source_geometry=source_geometry,
+        target_geometry=target_geometry,
+    )
 
-    # Use trained model to transport data
-    source_data_test = np.random.rand(n_test, n_voxels)
-    target_data_test = np.random.rand(n_test, n_voxels)
+    # Use trained model to transport new features
+    source_features_test = np.random.rand(n_test, n_voxels)
+    target_features_test = np.random.rand(n_test, n_voxels)
 
-    transformed_data = fugw.transform(source_data_test)
-    assert transformed_data.shape == source_data_test.shape
+    transformed_data = fugw.transform(source_features_test)
+    assert transformed_data.shape == source_features_test.shape
 
     # Compute score
-    s = fugw.score(source_data_test, target_data_test)
+    s = fugw.score(source_features_test, target_features_test)
     assert isinstance(s, int) or isinstance(s, float)
 
 
