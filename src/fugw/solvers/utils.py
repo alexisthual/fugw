2938866a45f3ee20b6593ae45cf33e9ef0710868
Alexis Thual <alexisthual@gmail.com>
2022-11-02 18:42:15 +0100

    Move low_rank_squared_l2 to correct utils file

--- src/fugw/solvers/utils.py
+++ src/fugw/solvers/utils.py
@@ -297,25 +297,6 @@ def solver_dc_sparse(
     return (u, v), pi
 
 
-def low_rank_squared_l2(X, Y):
-    """
-    Write square Euclidean distance matrix M as exact product
-    of two low rank matrices A1 and A2: M = A1 A2^t
-    """
-    device, dtype = X.device, X.dtype
-    nx, ny = X.shape[0], Y.shape[0]
-
-    Vx = (X**2).sum(1, keepdim=True)  # shape nx x 1
-    Vy = (Y**2).sum(1, keepdim=True)  # shape ny x 1
-    ones_x = torch.ones(nx, 1).to(device).to(dtype)  # shape nx x 1
-    ones_y = torch.ones(ny, 1).to(device).to(dtype)  # shape ny x 1
-
-    A1 = torch.cat([Vx, ones_x, -(2**0.5) * X], dim=1)  # shape nx x (d+2)
-    A2 = torch.cat([ones_y, Vy, 2**0.5 * Y], dim=1)  # shape ny x (d+2)
-
-    return (A1, A2)
-
-
 def compute_approx_kl(p, q):
     # By convention: 0 log 0 = 0
     entropy = torch.nan_to_num(
--- src/fugw/utils.py
+++ src/fugw/utils.py
@@ -18,3 +18,22 @@ def make_tensor(x):
         return x
     else:
         raise Exception(f"Expected np.array or torch.tensor, got {type(x)}")
+
+
+def low_rank_squared_l2(X, Y):
+    """
+    Write square Euclidean distance matrix M as exact product
+    of two low rank matrices A1 and A2: M = A1 A2^t
+    """
+    device, dtype = X.device, X.dtype
+    nx, ny = X.shape[0], Y.shape[0]
+
+    Vx = (X**2).sum(1, keepdim=True)  # shape nx x 1
+    Vy = (Y**2).sum(1, keepdim=True)  # shape ny x 1
+    ones_x = torch.ones(nx, 1).to(device).to(dtype)  # shape nx x 1
+    ones_y = torch.ones(ny, 1).to(device).to(dtype)  # shape ny x 1
+
+    A1 = torch.cat([Vx, ones_x, -(2**0.5) * X], dim=1)  # shape nx x (d+2)
+    A2 = torch.cat([ones_y, Vy, 2**0.5 * Y], dim=1)  # shape ny x (d+2)
+
+    return (A1, A2)
