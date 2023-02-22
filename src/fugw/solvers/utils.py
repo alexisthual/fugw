73303c080ef0bf5ecfabbf8f5752e0e0623c117e
Alexis Thual <alexisthual@gmail.com>
2023-02-23 00:31:25 +0100

    Fix flake8 errors

--- src/fugw/solvers/utils.py
+++ src/fugw/solvers/utils.py
@@ -130,8 +130,10 @@ def batch_elementwise_prod_and_sum(
     res = torch.cat(
         [
             (
-                X1[idx_1[i : i + batch_size].type(torch.LongTensor), :]
-                * X2[idx_2[i : i + batch_size].type(torch.LongTensor), :]
+                X1[idx_1[i : i + batch_size].type(torch.LongTensor), :]  # noqa
+                * X2[
+                    idx_2[i : i + batch_size].type(torch.LongTensor), :  # noqa
+                ]
             ).sum(axis)
             for i in range(0, m, batch_size)
         ]
