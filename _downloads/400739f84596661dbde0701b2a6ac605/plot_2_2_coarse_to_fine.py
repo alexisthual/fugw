9696440215e47ac7e19871a3b8d60c6f81558837
FUGW GitHub Action <fugw@github.com>
2023-02-27 11:10:30 +0000

    Update documentation

---
+++ _downloads/400739f84596661dbde0701b2a6ac605/plot_2_2_coarse_to_fine.py
@@ -0,0 +1,227 @@
+# %%
+"""
+Transport distributions using sparse solvers
+============================================
+
+In this example, we sample 2 toy distributions and compute
+a sparse fugw alignment between them.
+Sparse alignments are typically used when both aligned distributions
+have more than 10k points.
+"""
+
+# sphinx_gallery_thumbnail_number = 4
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+from fugw.scripts import coarse_to_fine
+from fugw.mappings import FUGW, FUGWSparse
+from fugw.mappings.utils import init_mock_distribution
+from matplotlib.collections import LineCollection
+from scipy.sparse import coo_matrix
+
+# %%
+np.random.seed(4)
+torch.manual_seed(0)
+
+n_points_source = 300
+n_samples_source = 100
+n_points_target = 300
+n_samples_target = 100
+n_features_train = 2
+n_features_test = 2
+
+# %%
+# Let us generate random training data for the source and target distributions
+_, source_features_train, _, source_embeddings = init_mock_distribution(
+    n_features_train, n_points_source
+)
+_, target_features_train, _, target_embeddings = init_mock_distribution(
+    n_features_train, n_points_target
+)
+
+# %%
+# We can visualize the generated features:
+fig = plt.figure(figsize=(4, 4))
+ax = fig.add_subplot()
+ax.set_title("Source and target features")
+ax.set_aspect("equal", "datalim")
+ax.scatter(source_features_train[0], source_features_train[1], label="Source")
+ax.scatter(target_features_train[0], target_features_train[1], label="Target")
+ax.legend()
+plt.show()
+
+# %%
+# Do not forget to normalize features and embeddings
+# before fitting the models.
+
+source_features_train_normalized = source_features_train / torch.linalg.norm(
+    source_features_train, dim=1
+).reshape(-1, 1)
+target_features_train_normalized = target_features_train / torch.linalg.norm(
+    target_features_train, dim=1
+).reshape(-1, 1)
+
+source_embeddings_normalized, source_d_max = coarse_to_fine.random_normalizing(
+    source_embeddings
+)
+target_embeddings_normalized, target_d_max = coarse_to_fine.random_normalizing(
+    target_embeddings
+)
+
+# %%
+# Let us define the coarse and fine-grained optimization problems to solve
+coarse_mapping = FUGW(alpha=0.5, eps=1e-4)
+fine_mapping = FUGWSparse(alpha=0.5, eps=1e-4)
+
+# %%
+# We also specify which solver to use at each of the 2 steps:
+coarse_mapping_fit_params = {
+    "uot_solver": "mm",
+    "tol_uot": 1e-10,
+}
+
+fine_mapping_fit_params = {
+    "uot_solver": "mm",
+    "tol_uot": 1e-10,
+}
+
+# %%
+# Now, let us fit both the coarse and fine-grained mappings.
+# The coarse mapping is fitted on a limited number
+# of points from the source and target distributions.
+# You should carefully set the source and target ``selection_radius``
+# as they will greatly affect the sparsity of the computed mappings.
+# They should usally be set using domain knowledge related to the distributions
+# you are trying to align.
+_, _ = coarse_to_fine.fit(
+    coarse_mapping=coarse_mapping,
+    coarse_mapping_fit_params=coarse_mapping_fit_params,
+    coarse_pairs_selection_method="topk",
+    source_selection_radius=1 / source_d_max,
+    target_selection_radius=1 / target_d_max,
+    fine_mapping=fine_mapping,
+    fine_mapping_fit_params=fine_mapping_fit_params,
+    source_sample_size=n_samples_source,
+    target_sample_size=n_samples_target,
+    source_features=source_features_train_normalized,
+    target_features=target_features_train_normalized,
+    source_geometry_embeddings=source_embeddings_normalized,
+    target_geometry_embeddings=target_embeddings_normalized,
+    verbose=True,
+)
+
+# %%
+# Both the coarse and fine-grained transport plans can be accessed
+# after the models have been fitted
+print(f"Coarse transport plan's total mass: {coarse_mapping.pi.sum():.5f}")
+print(
+    "Fine-grained transport plan's total mass:"
+    f" {torch.sparse.sum(fine_mapping.pi):.5f}"
+)
+
+# %%
+# Here is the evolution of the FUGW loss during training
+# of the coarse mapping, with and without the entropic term:
+
+fig, ax = plt.subplots(figsize=(4, 4))
+ax.set_title("Coarse mapping training loss")
+ax.set_ylabel("Loss")
+ax.set_xlabel("BCD step")
+ax.plot(coarse_mapping.loss_steps, coarse_mapping.loss_, label="FUGW loss")
+ax.plot(
+    coarse_mapping.loss_steps,
+    coarse_mapping.loss_ent,
+    label="FUGW entropic loss",
+)
+ax.legend()
+plt.show()
+
+# %%
+# And here is that of the fine-grained mapping:
+
+fig, ax = plt.subplots(figsize=(4, 4))
+ax.set_title("Fine-grained mapping training loss")
+ax.set_ylabel("Loss")
+ax.set_xlabel("BCD step")
+ax.plot(fine_mapping.loss_steps, fine_mapping.loss_, label="FUGW loss")
+ax.plot(
+    fine_mapping.loss_steps, fine_mapping.loss_ent_, label="FUGW entropic loss"
+)
+ax.legend()
+plt.show()
+
+# %%
+# In this example, the computed sparse transport plan is not very sparse:
+# it stores about 60% of what the equivalent dense transport plan
+# would store.
+# When aligning distributions with a high number of points,
+# we usually want to keep this number much smaller.
+
+sparsity_ratio = (
+    100 * fine_mapping.pi.values().shape[0] / fine_mapping.pi.shape.numel()
+)
+print(f"Ratio of non-null coefficients: {sparsity_ratio:.2f}%")
+
+# %%
+# We can also have a look at the sparsity mask of the
+# fine-grained transport plan. In this particular example,
+# we don't expect it to show a particularly meaningful structure.
+indices = fine_mapping.pi.indices()
+fine_mapping_as_scipy_coo = coo_matrix(
+    (
+        fine_mapping.pi.values(),
+        (indices[0], indices[1]),
+    ),
+    shape=fine_mapping.pi.size(),
+)
+
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.set_title("Sparsity mask of fine-grained mapping")
+ax.set_ylabel("Source vertices")
+ax.set_xlabel("Target vertices")
+plt.spy(fine_mapping_as_scipy_coo, precision="present", markersize=0.3)
+plt.show()
+
+# %%
+# We can observe the computed mappings between source
+# and target points in the feature space.
+pi = fine_mapping.pi.to_dense()
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot()
+ax.set_aspect("equal", "datalim")
+ax.set_title("Mapping\ndisplayed in feature space")
+
+# Draw lines between matched points
+indices = torch.cartesian_prod(
+    torch.arange(n_points_source), torch.arange(n_points_target)
+)
+segments = torch.stack(
+    [
+        source_features_train[:, indices[:, 0]],
+        target_features_train[:, indices[:, 1]],
+    ]
+).permute(2, 0, 1)
+pi_normalized = pi / pi.sum(dim=1).reshape(-1, 1)
+line_segments = LineCollection(
+    segments, alpha=pi_normalized.flatten(), colors="black", lw=1, zorder=1
+)
+ax.add_collection(line_segments)
+
+# Draw distributions
+ax.scatter(source_features_train[0], source_features_train[1], label="Source")
+ax.scatter(target_features_train[0], target_features_train[1], label="Target")
+
+ax.legend()
+plt.show()
+
+# %%
+# Finally, the fitted fine-grained model can transport unseen data
+# between source and target
+source_features_test = torch.rand(n_features_test, n_points_source)
+target_features_test = torch.rand(n_features_test, n_points_target)
+transformed_data = fine_mapping.transform(source_features_test)
+transformed_data.shape
+
+# %%
+assert transformed_data.shape == target_features_test.shape
---
+++ _downloads/545a55507fc13d508b1b0e8d6dd28609/plot_2_1_lmds.py
@@ -0,0 +1,100 @@
+# %%
+"""
+Generate embeddings from mesh
+=============================
+
+In this example, we show how to derive an embedding
+which approximates the kernel matrix of geodesic distances
+on a given mesh.
+This technique is useful when trying to align distributions
+with a large number of points. Indeed, the kernel matrix
+of pairwise distances won't fit in memory, but an embedding
+computed in the right dimension can probably estimate it.
+"""
+
+# sphinx_gallery_thumbnail_number = 1
+import gdist
+import matplotlib.pyplot as plt
+import meshzoo
+import numpy as np
+import torch
+
+from fugw.scripts import lmds
+
+# %%
+# Here, we will compute the exact geodesic distances from
+# each vertex to a random sample of ``n_landmarks`` vertices.
+# The derived embedding will be in dimension ``k``.
+torch.manual_seed(0)
+
+n_landmarks = 100
+k = 3
+
+# %%
+# Let us load a pre-computed mesh and have a look at it first
+coordinates, triangles = meshzoo.octa_sphere(10)
+coordinates.shape
+
+# %%
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(projection="3d")
+ax.plot_trisurf(
+    coordinates[:, 0],
+    coordinates[:, 1],
+    coordinates[:, 2],
+    triangles=triangles,
+)
+plt.show()
+
+# %%
+# Now, let's compute the embedding! This computation is easy to parallelize.
+X = lmds.compute_lmds(
+    coordinates,
+    triangles,
+    n_landmarks=n_landmarks,
+    k=k,
+    n_jobs=2,
+    verbose=True,
+)
+
+# %%
+# It should have the correct size
+assert X.shape == (coordinates.shape[0], k)
+
+# %%
+# We can actually have a peek at the computed embedding:
+
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(projection="3d")
+ax.set_title("Embedding approximating kernel matrix")
+ax.scatter(
+    X[:, 0],
+    X[:, 1],
+    X[:, 2],
+    s=15,
+)
+plt.show()
+
+# %%
+# Finally, we check that the exact matrix of geodesic distances
+# between pairs of vertices of the mesh
+# is well approximated by the kernel matrix derived from the embeddings:
+
+fig = plt.figure(figsize=(5, 10))
+
+ax = fig.add_subplot(211)
+ax.set_title("True matrix of geodesic distances")
+true_kernel_matrix = gdist.local_gdist_matrix(
+    coordinates.astype(np.float64),
+    triangles.astype(np.int32),
+).toarray()
+im = ax.imshow(true_kernel_matrix)
+plt.colorbar(im, ax=ax, shrink=0.9)
+
+ax = fig.add_subplot(212)
+ax.set_title("Approximated matrix of geodesic distances")
+approximated_kernel_matrix = torch.cdist(X, X)
+im = ax.imshow(approximated_kernel_matrix)
+plt.colorbar(im, ax=ax, shrink=0.9)
+
+plt.show()
