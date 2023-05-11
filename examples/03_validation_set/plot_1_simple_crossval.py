c53748aef9ef474af460e1c6ce0c04e4f576bc55
Pierre-Louis Barbarant <plbarbarant@mail.com>
2023-05-11 15:57:47 +0200

    Replace np slices with nilearn resample_img

--- examples/03_validation_set/plot_1_simple_crossval.py
+++ examples/03_validation_set/plot_1_simple_crossval.py
@@ -1,13 +1,14 @@
 # %%
 """
-===================================================================
-Align low-resolution brain volumes of 2 individuals with fMRI data
-===================================================================
+==================================================================
+Using a validation set to monitor the convergence of the fugw loss
+==================================================================
 
-In this example, we align 2 low-resolution brain volumes
-using 4 fMRI feature maps (z-score contrast maps).
+In this example, we use 3 fMRI feature maps for training and 2 independant
+fMRI feature maps for testing to examine the evolutions of a training and
+a validation loss on 2 low-resolution brain volumes.
 """
-# sphinx_gallery_thumbnail_number = 7
+# sphinx_gallery_thumbnail_number = 9
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -35,30 +36,33 @@ brain_data = datasets.fetch_localizer_contrasts(
     contrasts,
     n_subjects=n_subjects,
     get_anats=True,
+    get_masks=True,
 )
 
 source_imgs_paths = brain_data["cmaps"][0 : len(contrasts)]
 target_imgs_paths = brain_data["cmaps"][len(contrasts) : 2 * len(contrasts)]
+source_mask = brain_data["masks"][0]
 
 source_im = image.load_img(source_imgs_paths)
 target_im = image.load_img(target_imgs_paths)
+mask = image.load_img(source_mask)
 
 # %%
-# We then downsample the images by 3 to reduce the computational cost.
+# We then downsample the images by 5 to reduce the computational cost.
 SCALE_FACTOR = 5
 
-source_maps = np.nan_to_num(
-    source_im.get_fdata()[::SCALE_FACTOR, ::SCALE_FACTOR, ::SCALE_FACTOR]
-)
-target_maps = np.nan_to_num(
-    target_im.get_fdata()[::SCALE_FACTOR, ::SCALE_FACTOR, ::SCALE_FACTOR]
-)
+resized_source_affine = source_im.affine.copy() * SCALE_FACTOR
+resized_target_affine = target_im.affine.copy() * SCALE_FACTOR
 
-segmentation_fine = np.logical_not(np.isnan(source_im.get_fdata()[:, :, :, 0]))
-segmentation_coarse = segmentation_fine[
-    ::SCALE_FACTOR, ::SCALE_FACTOR, ::SCALE_FACTOR
-]
-coordinates = np.array(np.nonzero(segmentation_coarse)).T
+source_im_resized = image.resample_img(source_im, resized_source_affine)
+target_im_resized = image.resample_img(target_im, resized_target_affine)
+mask_resized = image.resample_img(mask, resized_source_affine)
+
+source_maps = np.nan_to_num(source_im_resized.get_fdata())
+target_maps = np.nan_to_num(target_im_resized.get_fdata())
+segmentation = mask_resized.get_fdata()
+
+coordinates = np.argwhere(segmentation > 0)
 
 source_features = source_maps[
     coordinates[:, 0], coordinates[:, 1], coordinates[:, 2]
@@ -132,5 +136,3 @@ ax2.tick_params(axis="y", labelcolor=color)
 plt.title("Training and validation losses")
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
 plt.show()
-
-# %%
